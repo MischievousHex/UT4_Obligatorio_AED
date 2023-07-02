5994e5d4ecd77ff5e9fbfcd1dd449d347bcafaf2
MischievousHex <santiagopanozzoa@outlook.com>
2023-07-01 23:05:49 -0300

    Funciona el método EncontrarOptimo,ArmarArbol se tiene que debuggear

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,16 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        System.out.println("Este programa no hace nada. Se necesita correr el archivo TestArbolBinario.");
+        Integer[] claves = {0, 10, 20, 30, 40};
+        int[] p = {0, 3, 3, 1, 1};
+        int[] q = {2, 3, 1, 1, 1};
+        TArbolBB<Integer> arbolOptimo = new TArbolBB<>();
+
+        TCalculadorMatricesOptimo<Integer> Calculador = new TCalculadorMatricesOptimo<>();
+        Calculador.EncontrarOptimo(4, p, q);
+        Calculador.ArmarArbol(0,3, claves, arbolOptimo);
+
+        System.out.println(arbolOptimo.inOrden());
 
     }
 }
--- src/TCalculadorMatricesOptimo.java
+++ src/TCalculadorMatricesOptimo.java
@@ -8,14 +8,14 @@ public class TCalculadorMatricesOptimo<T> implements ICalculadorMatricesOptimo
     @Override
     public void EncontrarOptimo(int cantidadElementos, int[] frecuenciaExito, int[] frecuenciaNoExito) {
 
-        this.C = new int[cantidadElementos][cantidadElementos];
-        this.W = new int[cantidadElementos][cantidadElementos];
-        this.R = new int[cantidadElementos][cantidadElementos];
+        this.C = new int[cantidadElementos+1][cantidadElementos+1];
+        this.W = new int[cantidadElementos+1][cantidadElementos+1];
+        this.R = new int[cantidadElementos+1][cantidadElementos+1];
 
         // Calcular C y W de la primera "linea" (00, 11, 22, 33, ...), todos tienen C = 0, W = q y R = 0
-        for(int i = 0; i < cantidadElementos; i++){
+        for(int i = 0; i <= cantidadElementos; i++){
             C[i][i] = 0;
-            W[i][i] = frecuenciaExito[i];
+            W[i][i] = frecuenciaNoExito[i];
             R[i][i] = 0;
         }
 
@@ -23,13 +23,14 @@ public class TCalculadorMatricesOptimo<T> implements ICalculadorMatricesOptimo
         for(int m = 0; m <= cantidadElementos; m++){ // m= 0, 1, 2, 3, 4
             for(int i = 0; i <= (cantidadElementos - m); i++){   // i = 0:4, 0:3, 0:2, 0:1, 0
                 int j = i + m;                                  // j = 0:4, 1:4, 2:4, 3:4, 4
-                if(j != 0)      // j == 0 ya está calculado
-                    // Calculo de W
-                    W[i][j] = W[i][j-1] + frecuenciaExito[j] + frecuenciaNoExito[j];
+                if(i == j)      // j == 0 ya está calculado
+                    continue;
+                // Calculo de W
+                W[i][j] = W[i][j-1] + frecuenciaExito[j] + frecuenciaNoExito[j];
 
         // Calcular C y R
                 // Obtener todos los Cs necesarios
-                int[] PosiblesC = new int[j-i+1];
+                int[] PosiblesC = new int[j-i];
                 int x = 0; // iterador
                 for(int k = i+1; k <= j; k++){
                     PosiblesC[x] = C[i][k-1] + C[k][j];
@@ -64,7 +65,7 @@ public class TCalculadorMatricesOptimo<T> implements ICalculadorMatricesOptimo
         // Insertamos el elemento actual
         TElementoAB<T> Elemento = new TElementoAB<T>(clave, (T) clave);
         elArbol.insertar(Elemento);
-        ArmarArbol(i, R[i][j]-1, claves, elArbol); // El hijo izquierdo va desde el menor valor a el de su padre -1
+        ArmarArbol(i, R[i][j-1], claves, elArbol); // El hijo izquierdo va desde el menor valor a el de su padre -1
         ArmarArbol(R[i][j], j, claves, elArbol); // El hijo derecho va desde su padre hasta el mayor valor.
     }
 }
